b2ee4869f6cd16e4f735d9aa123b0cdced1d0e46
erfan1908 <erfan.hosseiny@gmail.com>
2020-09-03 17:42:54 +0430

    dorosteh???

---
+++ js/mainjs.js
@@ -0,0 +1,27 @@
+$(".toggle-password").click(function() {
+
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+        input.attr("type", "text");
+    } else {
+        input.attr("type", "password");
+    }
+});
+
+function myFunction() {
+    var x = document.getElementById("pass");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+function myFunction2() {
+    var x = document.getElementById("pass");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
---
+++ js/slider.js
@@ -0,0 +1,63 @@
+﻿
+function go_right(el, step, lim, pos, i) {
+
+    if (pos[i] > -lim) {
+        pos[i] -= step;
+        el.style.right = pos[i] + "%";
+    }
+    // else if (pos[i] == -lim) {
+    //     pos[i] = 0;
+    //     el.style.right = 0 + "%";
+    // }
+}
+
+
+function go_left(el, step, lim, pos, i) {
+
+    if (pos[i] < 0) {
+        pos[i] += step;
+        el.style.right = pos[i] + "%";
+    }
+    else if (pos[i] == lim) {
+        pos[i] = 0;
+        el.style.right = 0 + "%";
+    }
+}
+
+
+// function go_left(el, step, pos, i) {
+//
+//     if (pos[i] < 0) {
+//         pos[i] += step;
+//         el.style.right = pos[i] + "%";
+//     }
+//
+// }
+
+
+
+
+// function move(i) {
+//     flag[0] = -100 * i;
+//     gal.style.left = flag[0] + "%";
+// }
+
+
+
+
+/*-------------*/
+// window.onscroll = function () {
+//
+//     if (window.pageYOffset > 200) {
+//         menu.className = "mob";
+//     }
+//     else {
+//         menu.className = "";
+//     }
+//
+// }
+
+flag = [0, 0, 0, 0, 0];
+
+// setInterval(" go_left(gal,100,200,flag,0)", 2000);
+
